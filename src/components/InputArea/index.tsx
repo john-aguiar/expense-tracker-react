c3a7ef09333eaac2e55675a83163767e5a93e181
John <johnaguiar.dev@gmail.com>
2022-06-08 23:08:13 -0300

    styling the buttons

--- src/components/InputArea/index.tsx
+++ src/components/InputArea/index.tsx
@@ -2,9 +2,13 @@ import * as C from './style'
 
 export const InputArea = () =>{
 
+    function handleAddIncome() {
+        console.log('add income')
+    }
+
     return(
         <C.Container>
-            <C.AddIncome> Add Novo Ganho</C.AddIncome>
+            <C.AddIncome onClick={handleAddIncome}>Add Novo Ganho</C.AddIncome>
             <C.AddExpense> Add Despesa </C.AddExpense>
         </C.Container>
     )
--- src/components/InputArea/style.ts
+++ src/components/InputArea/style.ts
@@ -8,12 +8,22 @@ export const Container =styled.div`
     box-shadow: 2px 5px 12px -2px #000000;
     margin-top: 20px;
     display: flex;
+
 `
 
 export const AddIncome = styled.button`
-    backgr
+    background-color: green;
+    padding: 1rem;
+    border: none;
+    border-radius: 1rem;
+    color: #fff;
+    margin-right: 1rem;
 `
 
 export const AddExpense = styled.button`
-
+    background-color: red;
+    padding: 1rem;
+    border: none;
+    border-radius: 1rem;
+    color: #fff;
 `
